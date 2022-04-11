bbf92f7afd880f3d63cda2102ad88d0e5a0b8965
SavvyDolphin77 <savvydolphin77@gmail.com>
2022-04-11 15:19:42 -0400

    v0.4.1 Customized Design for Back to Top of Page Button
    Updated Design of the Back to Top of Page Button in CSS to match color theme and alignment.
    Added a few comments to the javascript file for goToTop.js
    Added the space to left and right sections in the middle to define them more.

--- docs/js/goToTop.js
+++ docs/js/goToTop.js
@@ -4,9 +4,10 @@
 //Get the button:
 mybutton = document.getElementById("goToTopButton");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// calls the function below when user scrolls
 window.onscroll = function() {scrollFunction()};
 
+// funcion that if started scrolling a bit (20pxs worth) then displays the button
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
