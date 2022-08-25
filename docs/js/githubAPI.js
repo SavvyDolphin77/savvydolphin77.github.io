0c7ec192765504bb9dc4cfc36099ebef22cee0c2
SavvyDolphin77 <savvydolphin77@gmail.com>
2022-08-25 15:34:54 -0400

    v0.6.01 Added Comments to the GithubAPI script
    Added comments to provide better explanation of what program does.

--- docs/js/githubAPI.js
+++ docs/js/githubAPI.js
@@ -2,11 +2,16 @@
 // to display on the website page.
 
 const url = "https://api.github.com/users/savvydolphin77/repos?per_page=100";
+
+// set this number to current boxes one has on the page as elements
 var totalProjectsDisplay = 2;
 
+// pulls data from the url defined above
 $.get(url, function(data) {
 
+  // a for loop that goes and adds the content from the data based on the highest stars
   for(let j = 0; j < totalProjectsDisplay; j++) {
+    // below sorts and extracts data to variables for further use
     let sortedRepos = data.sort((a,b) => parseFloat(b.stargazers_count) - parseFloat(a.stargazers_count));
     let repoName = sortedRepos[j].name;
     let repoDescription = sortedRepos[j].description;
@@ -18,12 +23,13 @@ $.get(url, function(data) {
         languages.push(Object.keys(languageData)[i]);
       }
     });
+
+    // part of code that takes the data and adds it to each html element
     $(document).ready(function() {
       $("#repoLink" + j).attr('href', repoLink);
       $("#repoTitle" + j).html(repoName);
       $("#repoStars" + j).html(repoStars);
       $("#repoDescription" + j).html(repoDescription);
-      //New Code
       let languageList = $("#repoLanguageList" + j);
       setTimeout(function() {
         for(let i = 0; i < languages.length; i++) {
@@ -32,7 +38,4 @@ $.get(url, function(data) {
       });
     });
   }
-
-
-
 });
